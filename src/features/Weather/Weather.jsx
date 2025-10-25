e56caf89615d0183dc260abde89cc9022b9e9dc0
Paul Negoescu <paul.negoescu@nagarro.com>
2025-10-25 13:39:05 +0300

    Weather widget implemented with geolocation and search without filling the form from geolocation

--- src/features/Weather/Weather.jsx
+++ src/features/Weather/Weather.jsx
@@ -8,21 +8,59 @@ export function Weather() {
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+
+    function handleGeoSuccess({coords}) {
+      const lon = coords.longitude;
+      const lat = coords.latitude;
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1d260c5f4897b555ae217809965ad963`
+      )
+        .then((res) => res.json())
+        .then(setWeatherData);
+    }
+
+    function handleGeoError() {
+      console.warn("Geolocation didn't work");
+    }
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const jsonData = Object.fromEntries(formData.entries());
+    
     fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=Brasov,RO&appid=1d260c5f4897b555ae217809965ad963'
+      `https://api.openweathermap.org/data/2.5/weather?q=${jsonData.city},${jsonData.country}&appid=1d260c5f4897b555ae217809965ad963`
     )
       .then((res) => res.json())
       .then((d) => setWeatherData(d));
-  }, []);
-  
+  }
+
   return (
     <>
       <h1>Weather</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" name="city" />
+
+        <label htmlFor="country">Country</label>
+        <select name="country" id="country">
+          <option value="RO">Romania</option>
+          <option value="DE">Germany</option>
+          <option value="US">United States</option>
+        </select>
+
+        <button type="submit">Search</button>
+      </form>
+
       {!weatherData && <strong>Loading ...</strong>}
 
       {weatherData && (
         <>
-          <p><strong>The weather in Brasov is {weatherData.weather[0].main}!</strong></p>
+          <p><strong>The weather in {weatherData.name} is {weatherData.weather[0].main}!</strong></p>
           <output>{kelvinToCelsius(weatherData.main.temp)}&deg;C</output>
           <img width="50" src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} />
         </>
