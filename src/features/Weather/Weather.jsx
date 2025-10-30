e91f7e5efbb5ce11ef3bf91dcba3d11a55ba6942
Paul Negoescu <paul.negoescu@nagarro.com>
2025-10-30 10:17:00 +0200

    End of React course 2

--- src/features/Weather/Weather.jsx
+++ src/features/Weather/Weather.jsx
@@ -6,18 +6,28 @@ function kelvinToCelsius(degK) {
 
 export function Weather() {
   const [weatherData, setWeatherData] = useState(null);
+  const [inputValues, setInputValues] = useState({
+    city: '',
+    country: '',
+  });
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 
-    function handleGeoSuccess({coords}) {
+    function handleGeoSuccess({ coords }) {
       const lon = coords.longitude;
       const lat = coords.latitude;
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1d260c5f4897b555ae217809965ad963`
       )
         .then((res) => res.json())
-        .then(setWeatherData);
+        .then((serverData) => {
+          setWeatherData(serverData);
+          setInputValues({
+            city: serverData.name,
+            country: serverData.sys.country,
+          })
+        });
     }
 
     function handleGeoError() {
@@ -25,13 +35,16 @@ export function Weather() {
     }
   }, []);
 
+  console.log(weatherData);
+
   function handleSubmit(e) {
     e.preventDefault();
 
-    const form = e.target;
-    const formData = new FormData(form);
-    const jsonData = Object.fromEntries(formData.entries());
-    
+    // const form = e.target;
+    // const formData = new FormData(form);
+    // const jsonData = Object.fromEntries(formData.entries());
+    const jsonData = inputValues;
+
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${jsonData.city},${jsonData.country}&appid=1d260c5f4897b555ae217809965ad963`
     )
@@ -39,15 +52,34 @@ export function Weather() {
       .then((d) => setWeatherData(d));
   }
 
+  function handleInputChange(e) {
+    // const inputName = e.target.name;
+    // const newValues = {...inputValues};
+    // newValues[inputName] = e.target.value;
+    // setInputValues(newValues);
+    setInputValues({...inputValues, [e.target.name]: e.target.value});
+  }
+
   return (
     <>
       <h1>Weather</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" name="city" />
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={inputValues.city}
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="country">Country</label>
-        <select name="country" id="country">
+        <select
+          name="country"
+          id="country"
+          value={inputValues.country}
+          onChange={handleInputChange}
+        >
           <option value="RO">Romania</option>
           <option value="DE">Germany</option>
           <option value="US">United States</option>
@@ -60,9 +92,18 @@ export function Weather() {
 
       {weatherData && (
         <>
-          <p><strong>The weather in {weatherData.name} is {weatherData.weather[0].main}!</strong></p>
+          <p>
+            <strong>
+              The weather in {weatherData.name} is {weatherData.weather[0].main}
+              !
+            </strong>
+          </p>
           <output>{kelvinToCelsius(weatherData.main.temp)}&deg;C</output>
-          <img width="50" src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} />
+          <img
+            width="50"
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            alt={`Weather is ${weatherData.weather[0].main} icon`}
+          />
         </>
       )}
     </>
