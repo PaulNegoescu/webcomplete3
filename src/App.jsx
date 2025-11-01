ec95988fcf88be7a4d1756d474e05bbeb1a122ae
Paul Negoescu <paul.negoescu@nagarro.com>
2025-11-01 14:04:47 +0200

    End of React course 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import { TodoList } from "./features/Todos/TodoList";
 
 export function App() {
   return (
-    <TodoList />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Homepage</h1>} />
+        <Route path="todos" element={<TodoList />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/features/Todos/TodoList.jsx
+++ src/features/Todos/TodoList.jsx
@@ -1,15 +1,148 @@
+import { useEffect, useState } from 'react';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+const endpoint = `${apiUrl}/todos`;
+
+function processServerResponse(res) {
+  if (!res.ok) {
+    if (res.status === 404) {
+      console.warn('Resource not found!');
+    }
+
+    if (res.status === 401) {
+      console.warn('Please log in');
+    }
+
+    throw new Error('HTTP request error!');
+  }
+  return res.json();
+}
+
 export function TodoList() {
+  const [todos, setTodos] = useState(null);
+
+  // READ/RETRIEVE
+  useEffect(() => {
+    fetch(endpoint)
+      .then(processServerResponse)
+      .then((data) => setTodos(data))
+      .catch(console.warn);
+  }, []);
+
+  // if(!todos) {
+  //   return <strong>Loading ...</strong>;
+  // }
+
+  // let toDisplay = [];
+  // if (Array.isArray(todos)) {
+  //   //   for(const todo of todos) {
+  //   //     toDisplay.push(
+  //   //       <li key={todo.id}>
+  //   //         <label>
+  //   //           <input type="checkbox" /> {todo.title}
+  //   //         </label>
+  //   //       </li>
+  //   //     );
+  //   //   }
+
+  //   toDisplay = todos.map((todo) => (
+  //     <li key={todo.id}>
+  //       <label>
+  //         <input type="checkbox" /> {todo.title}
+  //       </label>
+  //     </li>
+  //   ));
+  // }
+
+  // CREATE
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const todoTitle = formData.get('title');
+
+    try {
+      const newTodo = await fetch(endpoint + 's', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: todoTitle,
+          completed: false,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(processServerResponse);
+
+      const newTodoList = [...todos, newTodo];
+      setTodos(newTodoList);
+      form.reset();
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  // DELETE
+  async function handleDeleteTodo(todo) {
+    // console.log(e.target.dataset.todoId);
+    // console.log(todo.id);
+
+    await fetch(`${endpoint}/${todo.id}`, { method: 'DELETE' });
+
+    const newTodoList = todos.filter((t) => t !== todo);
+    setTodos(newTodoList);
+  }
+
+  // UPDATE
+  async function handleCompleteTodo(todo) {
+    const updatedTodo = await fetch(`${endpoint}/${todo.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        completed: !todo.completed,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(processServerResponse);
+
+    const indexOfOldTodo = todos.findIndex((t) => t === todo);
+    const newTodoList = todos.toSpliced(indexOfOldTodo, 1, updatedTodo);
+
+    setTodos(newTodoList);
+  }
+
   return (
     <>
       <h1>Todos</h1>
-      <ul>
-        <li>
-          <label>
-            <input type="checkbox" />{' '}
-            Title
-          </label>
-        </li>
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">What do you want to do?</label>{' '}
+        <input type="text" id="title" name="title" />
+        <button type="submit">Add</button>
+      </form>
+      {!todos && <strong>Loading ...</strong>}
+      {todos && (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => handleCompleteTodo(todo)}
+                />{' '}
+                {todo.title}
+              </label>{' '}
+              <button
+                type="button"
+                data-todo-id={todo.id}
+                onClick={() => handleDeleteTodo(todo)}
+              >
+                &times;
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
@@ -17,7 +150,7 @@ export function TodoList() {
 // RESTful Web Api/Service (REpresentational State Transfer)
 // Resources:
 //    users, todos
-// Entity: 
+// Entity:
 //     1 user, 1 todo, (id) (resource)
 // Endpoint:
 // http://localhost:3000/todos/:id
